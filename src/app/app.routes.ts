27f2c781e490b168933380246980792da3e023aa
jmgrgo <jmgranadosgo@gmail.com>
2026-01-23 23:38:36 -0500

    chore: initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DashboardPage } from './features/dashboard/dashboard-page/dashboard-page';
+import { ShipmentsPage } from './features/shipments/shipments-page/shipments-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardPage },
+  { path: 'shipments', component: ShipmentsPage },
+];
